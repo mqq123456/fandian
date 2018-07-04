6cba6467bcb8f51921966f98e7e8c9b80b9cd48c
men <964978007@qq.com>
2018-07-04 17:11:07 +0800

    qianqiang
    提交一些文件

---
+++ DianDianPinZhuo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DianDianPinZhuo
+//
+//  Created by user on 15/10/21.
+//  Copyright © 2015年 fandiankeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+    
+}
